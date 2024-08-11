038c71f4c2401730bf1d287911ae981d168536be
Styles <facottry.dev@gmail.com>
2024-08-12 04:14:05 +0530

    Revert "Update scaleAuth.ts"
    This reverts commit ac1a13c6566f5e2798ea0ffa7e8fec2327674f0e.

--- client/src/lib/scaleAuth.ts
+++ client/src/lib/scaleAuth.ts
@@ -1,27 +1,19 @@
-export const generateScaleHash = async () => {
-  // Get the current date and time in UTC
+import crypto from "crypto";
+
+export const generateScaleHash = () => {
   const date = new Date();
-
-  // Define the permanent salt
-  const permanentSalt = "5adf9a7c3be84f966c00dc5c33a4a115f311eb1a962e540c0beccdc5d6d171d4";
-
-  // Create a temporary salt using the current UTC hour and rounded UTC minutes
   const currentHour = date.getUTCHours();
   const currentMinute = Math.floor(date.getUTCMinutes() / 5) * 5;
-  const temporarySalt = `${currentHour}${currentMinute}`;
-
-  // Generate a random number between 0 and 9
   const randomizer = Math.floor(Math.random() * 10);
+  const permanentSalt = process.env.NEXT_PUBLIC_SCALE_SALT;
 
-  // Concatenate the salts and the randomizer to form the data to hash
+  const temporarySalt = `${currentHour}${currentMinute}`;
   const dataToHash = `${permanentSalt}${temporarySalt}${randomizer}`;
 
-  // Generate the SHA-256 hash of the data
-  const hashBuffer = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(dataToHash));
+  const generatedHash = crypto
+    .createHash("sha256")
+    .update(dataToHash)
+    .digest("hex");
 
-  // Convert the hash buffer to a hexadecimal string
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-
-  return hashHex;
+  return generatedHash;
 };
